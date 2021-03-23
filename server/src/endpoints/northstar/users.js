519d12a7c8fdc609d432475af56c705a0adfcd1f
Aaron Schachter <aaronschachter@gmail.com>
2021-03-22 20:04:33 -0700

    Fixes bug in Users.getByMobile

--- server/src/endpoints/northstar/users.js
+++ server/src/endpoints/northstar/users.js
@@ -11,34 +11,40 @@ class UsersNorthstarEndpoint extends NorthstarEndpoint {
 
     this.baseUri = `${this.baseUri}/v2`;
   }
+
   /**
    * @param  {String} id
    * @param  {Object} query
    * @return {Promise}
    */
   get(id, query) { return this.executeGet(`${this.baseUri}/users/${id}`, query); }
+
   /**
    * @param  {String} email
    * @param  {Object} query
    * @return {Promise}
    */
   getByEmail(email, query) { return this.executeGet(`${this.baseUri}/email/${email}`, query); }
+
   /**
    * @param  {String} mobile
    * @param  {Object} query
    * @return {Promise}
    */
-  getByMobile(mobile) { return this.executeGet(`${this.baseUri}/mobile/${mobile}`, query); }
+  getByMobile(mobile, query) { return this.executeGet(`${this.baseUri}/mobile/${mobile}`, query); }
+
   /**
    * @param  {Object} data
    * @return {Promise}
    */
   create(data) { return this.executePost(`${this.baseUri}/users`, data); }
+
   /**
    * @param  {Object} query
    * @return {Promise}
    */
   index(query) { return this.executeGet(`${this.baseUri}/users`, query); }
+
   /**
    * @param  {String} id
    * @param  {Object} data
